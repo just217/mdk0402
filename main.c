8ea42c50b622a8d7b68ccaba66905a4c10e967b5
430 <glebatok@gmai.com>
2026-05-07 12:14:26 +0300

    ШЩПЛОШПКВЛШПВЛО

--- main.c
+++ main.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-
-#include "askname.h"
-
-int main(int argc, char **argv)
-{
-    char first[255], last[255];
-
-    askname(first, last);
-
-    printf("Hello, %s %s!\n", first, last);
-    return 0;
-}
-
-  
+paketikiАЩАШЩАШЫЩЗШЫЩЗШРЩЫЗШПНЩЗВШНКЩЗ
